9a1af6c3aa5b639ce1133b757a7b4ebba7403a5b
Daffa Hardhan <dapahardan@gmail.com>
2025-12-12 20:38:17 +0700

    portofolio-1.0

--- fe/service/api.ts
+++ fe/service/api.ts
@@ -8,7 +8,7 @@
  */
 
 // 1. Config: Gunakan Environment Variable untuk fleksibilitas deploy
-const API_BASE = process.env.NEXT_PUBLIC_API_URL
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL
 
 // || 'http://localhost:5000/api'
 
